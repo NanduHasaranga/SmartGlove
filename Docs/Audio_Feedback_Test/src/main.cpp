bcc066443b856c0dc950294c55459aac40fda2b0
Nandu <razergammer201@gmail.com>
2024-09-26 23:18:28 +0530

    print update

--- Docs/Audio_Feedback_Test/src/main.cpp
+++ Docs/Audio_Feedback_Test/src/main.cpp
@@ -4,8 +4,6 @@ int flexPin1 = 34;  // Analog pin where the sensor is connected
 int flexPin2 = 35;
 int flexValue1, flexValue2;
 
-
-
 void setup() {
   Serial.begin(115200);
   analogReadResolution(12);
@@ -15,6 +13,7 @@ void loop() {
   flexValue1 = analogRead(flexPin1);
   flexValue2 = analogRead(flexPin2);
   Serial.println(flexValue1);  // Print the sensor value to the Serial Monitor
+  Serial.print(",");
   Serial.println(flexValue2);
   delay(1000);
 }
