fc96f8c029c06bc203653582617f1f9caf443cb3
Nandu <razergammer201@gmail.com>
2024-10-10 22:51:25 +0530

    sound pattern change

--- Docs/Audio_Feedback_Test/src/sound.py
+++ Docs/Audio_Feedback_Test/src/sound.py
@@ -2,7 +2,6 @@ import serial
 import pygame
 import time
 
-# Initialize the Pygame mixer
 pygame.mixer.init()
 
 # Load audio tracks
@@ -10,7 +9,6 @@ audio1 = 'src\SoundTrack\One_Sound.mp3'
 audio2 = 'src\SoundTrack\Two_Sound.mp3'
 audio3 = 'src\SoundTrack\Three_Sound.mp3'
 
-# Open the serial port
 ser = serial.Serial('COM3', 115200, timeout=1)
 
 is_playing = False
@@ -34,18 +32,18 @@ while True:
                 print(f"Sensor1: {sensor1_value}, Sensor2: {sensor2_value}")
 
                 # Determine which audio to play based on the sensor values
-                if sensor1_value < 1000 and sensor2_value > 1000 and not pygame.mixer.music.get_busy():
-                    play_audio(audio1)
-                    is_playing = True
-                    print(1);
-                elif sensor1_value > 1000 and sensor2_value < 1000 and not pygame.mixer.music.get_busy():
-                    play_audio(audio2)
-                    is_playing = True
-                    print(2);
-                elif sensor1_value < 1000 and sensor2_value < 1000 and not pygame.mixer.music.get_busy():
+                if sensor1_value < 1300 and sensor2_value < 1300 and not pygame.mixer.music.get_busy():
                     play_audio(audio3)
                     is_playing = True
                     print(3);
+                elif sensor1_value > 1300 and sensor2_value < 1300 and not pygame.mixer.music.get_busy():
+                    play_audio(audio2)
+                    is_playing = True
+                    print(2);
+                elif sensor1_value < 1300 and sensor2_value > 1300 and not pygame.mixer.music.get_busy():
+                    play_audio(audio1)
+                    is_playing = True
+                    print(1);
 
         # Reset is_playing flag when music stops
         if is_playing and not pygame.mixer.music.get_busy():
@@ -56,6 +54,6 @@ while True:
     except KeyboardInterrupt:
         break
 
-# Stop the music and close the serial port on exit
 pygame.mixer.music.stop()
 ser.close()
+ 
